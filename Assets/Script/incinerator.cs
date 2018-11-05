1d0c2761461c3ff0a993a0d7de2ce5b16dc6df20
Salvador Galindo <sagalind@ucsc.edu>
2018-11-05 11:00:41 -0800

    added incinerator model and starting (unfinished) code for incinerating

---
+++ Assets/Script/incinerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class incinerator : MonoBehaviour {
+
+    public bool incinerate = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("enter"))
+        {
+            if (!incinerate)
+            {
+                incinerate = true;
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (incinerate)
+        {
+            if (other.gameObject.tag == "Destructable")
+            {
+                Destroy(other);
+            }
+        }
+    }
+}
