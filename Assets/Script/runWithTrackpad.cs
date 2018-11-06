34d04220b1eaff30452f6258da57b67d36e07f2a
DHalsey <dhalsey@ucsc.edu>
2018-11-05 23:56:40 -0800

    changed player movement

--- Assets/Script/runWithTrackpad.cs
+++ Assets/Script/runWithTrackpad.cs
@@ -5,20 +5,27 @@ using Valve.VR;
 
 public class runWithTrackpad : MonoBehaviour {
     private Vector2 trackPadLeftPos;
+    private bool trackPadLeftTap = false;
     public float acceleration = 1000.0f;
+    public Transform headTransform;
+    private Quaternion headForward; //used to handle the direction of the movement to be in relation of direction that the player is looking
     private float maxSpeed = 2.0f;
     private Rigidbody rb; // the rigidbody of the player
-    public SteamVR_Action_Vector2 touchPadAction;
+
+    public int test = 1; //A variable ONLY for testing (for toggles)
+
+    private SteamVR_Action_Vector2 touchPadAction; //the position of the touched touch pad
+    private SteamVR_Action_Boolean touchPadTap; //if the touch pad is being touched
 	// Use this for initialization
 	void Start () {
         rb = this.gameObject.GetComponent<Rigidbody>();
+        touchPadAction = SteamVR_Input._default.inActions.TouchpadTouch;
+        touchPadTap = SteamVR_Input._default.inActions.TouchpadTap;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
         GetControls();
-        
 	}
 
     private void FixedUpdate() {
@@ -29,14 +36,26 @@ public class runWithTrackpad : MonoBehaviour {
     }
 
     void GetControls() {
-        trackPadLeftPos = touchPadAction.GetAxis(SteamVR_Input_Sources.Any);
-        Debug.Log(trackPadLeftPos);
+        trackPadLeftPos = touchPadAction.GetAxis(SteamVR_Input_Sources.LeftHand);
+        if(test == 1) Test();
+        if(test == 2) Test2();
 
+    }
+
+    void Test() {
+        if (touchPadTap.GetStateDown(SteamVR_Input_Sources.LeftHand)) {
+            headForward = headTransform.rotation;
+            Debug.Log("Touched");
+        }
+    }
+
+    void Test2() {
+        headForward = headTransform.rotation;
     }
 
     void MovePlayer() {
         Vector3 direction = new Vector3(trackPadLeftPos.x, 0, trackPadLeftPos.y);
-        rb.AddForce(direction*acceleration*Time.deltaTime);
+        rb.AddForce(headForward*direction*acceleration*Time.deltaTime);
     }
 
     //prevents the player from moving faster than the maximum speed
--- Assets/SteamVR_Input/ActionObjects/SteamVR_Input_ActionSet_default_In.cs
+++ Assets/SteamVR_Input/ActionObjects/SteamVR_Input_ActionSet_default_In.cs
@@ -32,5 +32,7 @@ namespace Valve.VR
         public Valve.VR.SteamVR_Action_Skeleton SkeletonRightHand;
         
         public Valve.VR.SteamVR_Action_Vector2 TouchpadTouch;
+        
+        public Valve.VR.SteamVR_Action_Boolean TouchpadTap;
     }
 }
