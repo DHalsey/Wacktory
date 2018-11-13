81eed9cb271ab95ead8d0d65c9b6b4ed9bda61c6
Talon Baker <taalonn@gmail.com>
2018-11-12 17:36:00 -0800

    Completely working and not broken XRAY machine

---
+++ Assets/Script/talon_boxContents.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class talon_boxContents : MonoBehaviour
+{
+
+    public GameObject chickenObj;
+    public GameObject bombObj;
+    public GameObject walnutObj;
+    public int numItems;
+
+    List<GameObject> boxObjsArray = new List<GameObject>();
+
+    GameObject heldContent;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Adding all the items to the list
+        boxObjsArray.Add(chickenObj);
+        boxObjsArray.Add(bombObj);
+        boxObjsArray.Add(walnutObj);
+
+        // Putting random object in a box
+        heldContent = boxObjsArray[Random.Range(0, numItems)];
+    }
+
+    // Displays the contints 
+    public void showContents() {
+        heldContent = Instantiate(heldContent, transform.position, transform.rotation);
+    }
+
+    public void updateContentsPos(Vector3 newPos) {
+        heldContent.transform.position = newPos;
+    }
+
+    public void hideContents() {
+        Destroy(heldContent);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
