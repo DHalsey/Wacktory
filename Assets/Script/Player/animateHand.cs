62e67e5c92cba0f7e581d3121ab0008d3ec47365
DHalsey <dhalsey@ucsc.edu>
2018-11-16 20:25:32 -0800

    cleaned up and commented code

--- Assets/Script/Player/animateHand.cs
+++ Assets/Script/Player/animateHand.cs
@@ -1,4 +1,15 @@
-﻿using System.Collections;
+﻿//Dustin Halsey - dhalsey
+//animateHand.cs
+//Functionality: 
+//  This script is intended to control the animation of the VR person's hands whenever the trigger on the controller is pulled
+//  The boolean "isLeftHand" is used to swap functionality of this script between hands
+//  The hand object MUST have an animator on it
+//How to Attach:
+//  Attach this to the hand model that is a child of the controller objects.
+//  Public Variables:
+//      isLeftHand - set this to be accurate to the hand you are using
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
@@ -6,9 +17,9 @@ using Valve.VR;
 public class animateHand : MonoBehaviour {
     //used to get which controller this script is using. Initialized to any hand
     //in case we do not find the hand properly
-    private SteamVR_Input_Sources hand = SteamVR_Input_Sources.Any;
-    private Animator anim;
-    public bool isLeftHand = false;
+    private SteamVR_Input_Sources hand = SteamVR_Input_Sources.Any; //keeps track of which controller this script should use.  Initialized to "any" to ensure that it should work if even if the input is funky
+    private Animator anim; //the animator that controls the hand animation
+    public bool isLeftHand = false; //decides what controller "hand" will be
 
     void Start () {
         anim = GetComponent<Animator>();
@@ -19,15 +30,19 @@ public class animateHand : MonoBehaviour {
         } else if (isLeftHand == false) {
             hand = SteamVR_Input_Sources.RightHand;
         }
-        if (hand == SteamVR_Input_Sources.Any) {
-            Debug.Log("Warning: Hand not found for animateHand script.  Defaulting to any controller input");
-        }
     }
-    // Update is called once per frame
+    
+    //updates the animation of the hand.  This may be expanded on in the future if we need other hand animations
     void Update () {
-        AnimatorClipInfo[] currentClipInfo = anim.GetCurrentAnimatorClipInfo(0); //get the first clip on layer 0 of animations
-        float triggerPullAmount = SteamVR_Input._default.inActions.GrabTrigger.GetAxis(hand);
-        Debug.Log(triggerPullAmount);
-        anim.Play(currentClipInfo[0].clip.name.ToString(), 0, triggerPullAmount);
+        //gets an array of all clips.  In this case, there will only be 1 clip to animate so that is the only one we care about
+        //the zero is to specify the animation layer to pull from.  This is a basic animation so only 1 animation should be present atm
+        AnimatorClipInfo[] currentClipInfo = anim.GetCurrentAnimatorClipInfo(0); 
+        float triggerPullAmount = SteamVR_Input._default.inActions.GrabTrigger.GetAxis(hand); //the amount from 0-1 that the trigger is currently pulled
+
+        string clipToPlay = currentClipInfo[0].clip.name.ToString(); //get the name of the clip we want to play
+
+        //always update current time in the animation to match the controller's trigger pull amount
+        //note that the time of an animation is normalized from 0-1 so the triggerPullAmount works perfectly
+        anim.Play(clipToPlay, 0, triggerPullAmount); 
     }
 }
--- Assets/Script/Player/vrHitboxFollowHMD.cs
+++ Assets/Script/Player/vrHitboxFollowHMD.cs
@@ -1,4 +1,13 @@
-﻿//Attach this to the vrPlayer GameObject
+﻿//Dustin Halsey - dhalsey
+//vrHitboxFollowHMD.cs
+//Functionality: 
+//  have the hitbox of the player follow the headset's position and height
+//
+//How to Attach:
+//  Attach this to the playerVR gameObject
+//  Public Variables:
+//      HMD - the gameobject for the head (camera)
+//      playerCollider - the collider for the player
 
 using System.Collections;
 using System.Collections.Generic;
@@ -19,7 +28,8 @@ public class vrHitboxFollowHMD : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        playerCollider.center = new Vector3(HMD.transform.localPosition.x, HMD.transform.localPosition.y/2, HMD.transform.localPosition.z);
+        //set the center of the collider halfway between the headset height and the floor
+        playerCollider.center = new Vector3(HMD.transform.localPosition.x, HMD.transform.localPosition.y/2, HMD.transform.localPosition.z); 
         playerCollider.height = HMD.transform.localPosition.y;
 
     }
