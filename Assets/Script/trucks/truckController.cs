fd23f2f75c0fa2fb5656f37f5c6f0e372b36d537
JoshuaClouse <jclouse@ucsc.edu>
2019-01-13 21:43:40 -0800

    still sorting out some bug when dequing trucks.

---
+++ Assets/Script/trucks/truckController.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class truckController : MonoBehaviour
+{
+
+    private Truck thisTruck;
+    public GameObject truckManager;
+    public GameObject windowStop;
+    public GameObject loadingStop;
+    public GameObject truck_end;
+    //list of all the possible items
+    private List<string> itemList = new List<string>();
+    //total # of items the truck wants
+    private int numItems;
+    //a string of items the truck wants
+    private string[] groceryList;
+    //how long the truck is willing to wait
+    private int waitTime;
+    //start time for lerping purposes and distance
+    private float startTime;
+    float journeyLength;
+    private float speed = .05f;
+    //if the object is moving towards the window/loading bay
+    private bool window = false;
+    private bool loading = false;
+    private bool end = false;
+    private string truckStatus = "start";
+    // Use this for initialization
+
+    void Start()
+    {
+        //on creation we want to create a random item list for the truck to need
+        //first we make a list of all possible items
+        itemList.Add("banana");
+        itemList.Add("bomb");
+        //now we can choose how many items we want and then randomly select items from the list for testing it will be set to 5
+        numItems = 5;
+        int rand;
+        groceryList = new string[numItems];
+        for (int i = 0; i < numItems; i++)
+        {
+            //picks a random index from the total items list and puts that item on the grocery list
+            //Debug.Log(items.Count);
+            rand = Random.Range(0, itemList.Count);
+            groceryList[i] = itemList[rand];
+        }
+
+        //we can assign this a random value but for testing we will make it a standard 10 second wait time
+        waitTime = 10;
+        //at this point we have all the components for the construction of the truck
+        thisTruck = new Truck(waitTime, groceryList, numItems);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //in here will put some code to detect what items have been given to the truck and what not
+        if (window == true)
+        {
+            truckStatus = "moving";
+            //Debug.Log("window is true and start");
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            transform.position = Vector3.Lerp(this.transform.position, windowStop.transform.position, fracJourney);
+            //Debug.Log(Vector3.Distance(transform.position, windowStop.transform.position));
+            if (Vector3.Distance(transform.position, windowStop.transform.position) < .5f)
+            {
+                window = false;
+                //truckManager.GetComponent<truck_manager>().changeMovingStatus();
+                truckStatus = "ordering";
+                startTime = Time.time;
+            }
+        }
+        else if (truckStatus == "ordering")
+        {
+            //Debug.Log("status is ordering");
+            if (Time.time - startTime > 5f)
+            {
+                truckStatus = "waitForLoad";
+            }
+        }
+        else if(loading == true)
+        {
+            truckStatus = "moving";
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            transform.position = Vector3.Lerp(this.transform.position, loadingStop.transform.position, fracJourney);
+            if (Vector3.Distance(transform.position, loadingStop.transform.position) < .5f)
+            {
+                loading = false;
+                //truckManager.GetComponent<truck_manager>().changeMovingStatus();
+                truckStatus = "loading";
+                startTime = Time.time;
+            }
+        }
+        else if(truckStatus == "loading")
+        {
+            if(Time.time - startTime > waitTime)
+            {
+                truckStatus = "complete";
+            }
+        }
+        else if(end == true)
+        {
+            truckStatus = "moving";
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            transform.position = Vector3.Lerp(this.transform.position, truck_end.transform.position, fracJourney);
+            if (Vector3.Distance(transform.position, truck_end.transform.position) < .5f)
+            {
+                end = false;
+                //truckManager.GetComponent<truck_manager>().changeMovingStatus();
+                truckStatus = "delete";
+            }
+        }
+        else if(truckStatus == "delete")
+        {
+            truckManager.GetComponent<truck_manager>().deleteTruck();
+            //Destroy(this.gameObject);
+        }
+    }
+
+    public void setMoveToWindow()
+    {
+        //Debug.Log("setting window to true");
+        window = true;
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(this.transform.position, windowStop.transform.position);
+        //Debug.Log(journeyLength);
+    }
+    public void setMoveToLoading()
+    {
+        loading = true;
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(this.transform.position, loadingStop.transform.position);
+    }
+    public void setMoveToEnd()
+    {
+        end = true;
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(this.transform.position, truck_end.transform.position);
+    }
+    public string getStatus()
+    {
+        return truckStatus;
+    }
+}
---
+++ Assets/Script/trucks/truck_manager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class truck_manager : MonoBehaviour {
+
+    //variables used to hold timers to determine when to send a new truck
+    private float time1;
+    private float time2;
+    //the total amount of trucks we want to have on the map at a time
+    private int maxTruckCount;
+    private int currentTruckCount;
+    //a listcontaining all possible items
+    //private List<string> items = new List<string>();
+    public GameObject truck;
+    //queue for holding the trucks
+    private Queue<GameObject> truckQueue = new Queue<GameObject>();
+    private GameObject[] truckQueueCopy;
+    //truck stops
+    public GameObject first_stop;
+    public GameObject second_stop;
+    private bool changeInTruck;
+
+	// Use this for initialization
+	void Start () {
+        //set the current time as well as the max truck count
+        time1 = Time.time;
+        maxTruckCount = 3;
+        currentTruckCount = 0;
+
+        //makes an list of strings for all the possible items this can be changed later to something else for now just hard
+        //coding it in
+        //items.Add("banana");
+        //items.Add("bomb");
+        //Debug.Log(items.Count);
+        //we need an array that can store a copy of the truck queue so that we can reference the trucks that aren't at the top
+        //of the queue
+        //truckQueue = new Queue<GameObject>();
+        truckQueueCopy = new GameObject[maxTruckCount];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //have a variable that checks to see if we manipulated the queue in any way (truck added or deleted)
+        changeInTruck = false;
+        //update time2
+        time2 = Time.time;
+        //if 10 seconds have passed in the game and we have less than the max trucks spawn a truck in
+        if(time2- time1 >= 8 && currentTruckCount < maxTruckCount)
+        {
+            //reset time1 to be current time
+            time1 = Time.time;
+            GameObject newTruck = Instantiate(truck, this.transform.position, Quaternion.identity) as GameObject;
+            //once the new truck has been created we add it to the truck queue and add 1 to truck count
+            truckQueue.Enqueue(newTruck);
+            currentTruckCount++;
+            changeInTruck = true;
+        }
+        //put something here for when trucks leave
+
+        //if a truck was added to the queue or popped off we need to recreate the array that holds a copy of the queue
+        //this is so we don't copy the array every single frame and will help optimization
+        if(changeInTruck == true)
+        {
+            truckQueueCopy = truckQueue.ToArray();
+        }
+        
+        //Debug.Log(currentTruckCount);
+        //just check if the first truck needs to be moved and if so run moveTrucks otherwise it's pointless
+        if(truckQueue.Count > 0)
+        {
+            Debug.Log(truckQueue.Count);
+            string status = truckQueueCopy[0].GetComponent<truckController>().getStatus();
+            //Debug.Log(status);
+            if (status == "start" || status == "waitForLoad" || status == "complete")
+            {
+                moveTrucks();
+            }
+        }
+        //Debug.Log(currentTruckCount);
+        
+	}
+
+    void moveTrucks()
+    {
+        //dealing with edge cases first, if this is the first truck
+        for(int i = 0; i < truckQueue.Count; i++)
+        {
+   
+            if (truckQueueCopy[i].GetComponent<truckController>().getStatus() == "start")
+            {
+                truckQueueCopy[i].GetComponent<truckController>().setMoveToWindow();
+            }
+            else if(truckQueueCopy[i].GetComponent<truckController>().getStatus() == "waitForLoad")
+            {
+                truckQueueCopy[i].GetComponent<truckController>().setMoveToLoading();
+            }
+            else if(truckQueueCopy[i].GetComponent<truckController>().getStatus() == "complete")
+            {
+                truckQueueCopy[i].GetComponent<truckController>().setMoveToEnd();
+            }
+        }
+    }
+
+    public void deleteTruck()
+    {
+        truckQueue.Dequeue();
+        changeInTruck = true;
+    }
+    //public List<string> getItemList()
+    //{
+        //Debug.Log(items.Count);
+        //return items;
+    //}
+}
