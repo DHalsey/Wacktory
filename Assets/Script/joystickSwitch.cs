e2293e4a0c9e05ec9bb3687b9cb683f7d8abe959
Reed Scriven <r433scriven@gmail.com>
2019-01-15 03:03:27 -0800

    made my own scene and have started to code up the switch

---
+++ Assets/Script/joystickSwitch.cs
@@ -0,0 +1,39 @@
+﻿//Reed Scriven - SolSearcher
+//joystickSwitch.cs
+//
+//Functionality: 
+//  Allows the player to control and use the joystick to control an outcome in the game
+//
+//How to Attach:
+//  tbd
+//
+//Public Variables:
+//  tbd
+
+using UnityEngine;
+
+public class joystickSwitch : MonoBehaviour{
+    //Initialize
+    public Material joystickGlow; // the accent color on the joystick
+    public Color colorActive; // what color the accent will turn when in use
+    private Color colorInactive; // what color the accent will be at rest
+
+
+
+
+    // Start is called before the first frame update
+    void Start(){
+
+         colorInactive = joystickGlow.color;
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    public void interact() {
+        print("Interacting with Joystick");
+    }
+}
