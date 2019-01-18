bff14aecad6eb2b9f4b1a09a94ee7460b406d298
Salvador Galindo <sagalind@ucsc.edu>
2019-01-17 22:15:07 -0800

    finished variable throw, as well as a line renderer that indicates how strong a throw will be. Simply hold the throw button to increase the force. There is a small visual bug that makes the line appear at the wrong position for a frame. I'm not sure how to fix it.

--- Assets/Script/couchPlayer/couchPlayerPickUp.cs
+++ Assets/Script/couchPlayer/couchPlayerPickUp.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class couchPlayerPickUp : MonoBehaviour {
 
-    public float throwForce = 10.0f;
-    public float pickupCooldown = 1.0f;
+    public float minThrowForce = 3.0f; // Minimum force with which an item would be thrown
+    public float maxThrowForce = 10.0f; // Maximum force with which an item would be thrown
+    public float chargeRate = 0.1f; // Rate at which a throw is charged
+    private float throwForce; // How hard the player would throw if the throw button was released
+    private bool charging = false; // If a throw is being charged or not
+
+    public float pickupCooldown = 1.0f; // Cooldown for picking up items (to avoid weird collision glitches)
 
     private float timestamp; // Timestamp for calculating pick up cooldown
 
@@ -33,6 +38,8 @@ public class couchPlayerPickUp : MonoBehaviour {
         // Input names for holding and throwing objects
         holdButtonName = "Hold" + transform.parent.GetComponent<couchPlayerMovement>().playerNumber;
         throwButtonName = "Throw" + transform.parent.GetComponent<couchPlayerMovement>().playerNumber;
+
+        throwForce = minThrowForce;
 	}
 	
 	// Update is called once per frame
@@ -49,29 +56,32 @@ public class couchPlayerPickUp : MonoBehaviour {
             pickup = true;
         }
 
-        // If we start to ragdoll while holding an item, let go of the item
-        if (ragdolling && heldItem != null)
+        // If we are holding an item, check for release/throw conditions
+        if (heldItem != null)
         {
-            Release();
-        }
-
-        // If we let go of the hold button and we are holding an item, let go of it.
-        if (holdButtonInput == 0 && heldItem != null)
-        {
-            Release();
-        }
-
-        // If the throw button is pressed
-        if (throwButtonInput > 0.0f && heldItem != null)
-        {
-            Throw();
+            // If the hold button is released or we start ragdolling, let go of the held item
+            if (holdButtonInput == 0 || ragdolling)
+            {
+                Release();
+            }
+            // If we press the throw button, start charging the throw. 
+            // Else, if we are not pressing the throw button but charging is still true (AKA we let go of the throw button), throw item.
+            if (throwButtonInput > 0.5f)
+            {
+                charging = true;
+                ChargeThrow();
+            }
+            else if (charging == true)
+            {
+                Throw();
+            }
         }
 	}
 
     // Check if there is a grabbable object in front of us.
     private void OnTriggerStay(Collider other)
     {
-        // If we're pressing the hold button, we're not already holding an item and this item is grabbable, pick it up.
+        // If we're pressing the hold button, we're not already holding an item, and this item is grabbable, pick it up.
         if (pickup && heldItem == null && other.gameObject.tag == "grabbable")
         {
             PickUp(other.gameObject);
@@ -90,11 +100,15 @@ public class couchPlayerPickUp : MonoBehaviour {
             // Change the holdPosition's transform to fit the item's size, plus a little bit of padding
             holdPosition.transform.position = transform.parent.position + transform.parent.forward * ((transform.parent.GetComponent<Collider>().bounds.size.z / 2)
                 + (heldItem.GetComponent<Collider>().bounds.size.z / 2) + 0.05f);
-            heldItem.transform.rotation = holdPosition.rotation;
+            heldItem.transform.rotation = holdPosition.rotation; // Set itme's rotation to holdPosition's
             heldItem.transform.position = holdPosition.position; // Place item in the holdPosition
-            heldItem.GetComponent<Collider>().material = heldItemPhysicMaterial;
+            heldItem.GetComponent<Collider>().material = heldItemPhysicMaterial; // Set item's physics material to one without friction and bounciness
 
+            // Item ignores collision with player so that there aren't any crazy game-breaking rigidbody bugs like being launched at the speed of light
             Physics.IgnoreCollision(holdPosition.transform.parent.GetComponent<Collider>(), heldItem.GetComponent<Collider>(), true);
+
+            // To actually hold the object and use still use rigidbody physics, the item is held in place (holdPosition) with a FixedJoint. This tells Unity to keep calculating
+            // heldItem's rigidbody physics and to not force it through other objects like walls (something that would happen if we set it as a chiled of holdPosition instead)
             heldItem.AddComponent<FixedJoint>();
             FixedJoint fixJoint = heldItem.GetComponent<FixedJoint>();
             fixJoint.GetComponent<FixedJoint>().connectedBody = holdPosition.GetComponent<Rigidbody>();
@@ -104,18 +118,26 @@ public class couchPlayerPickUp : MonoBehaviour {
 
     private void Release()
     {
+        // Destroy the FixedJoint that held the item in place
         pickup = false;
         if (joined)
         {
             Destroy(heldItem.GetComponent<FixedJoint>());
             joined = false;
         }
+
+        // Reset the item's physics material to null (resets its friction and bounciness)
         heldItem.GetComponent<Collider>().material = null;
+
+        // Re-enable collision with player
         Physics.IgnoreCollision(holdPosition.transform.parent.GetComponent<Collider>(), heldItem.GetComponent<Collider>(), false);
-        // Revert holdPosition back to where it was before picking up the item
+        
+        // Revert holdPosition back to where it was
         holdPosition.transform.position = transform.parent.position + transform.parent.forward * (transform.parent.GetComponent<Collider>().bounds.size.z / 2);
         Rigidbody rbItem = heldItem.GetComponent<Rigidbody>();
-        rbItem.constraints = RigidbodyConstraints.None; // Disable item's rigidbody constraints so its physics are back to normal
+
+        Debug.Log("Released: " + heldItem.gameObject.name);
+
         heldItem = null; // Set heldItem back to null
 
         timestamp = Time.time + pickupCooldown; // Take a timestamp of when the item was released in order to check the pickup cooldown
@@ -130,6 +152,63 @@ public class couchPlayerPickUp : MonoBehaviour {
             // Throw the item in the player's forward direction at throwForce strength
             rbItem.AddForce(transform.parent.forward * throwForce, ForceMode.Impulse);
         }
+        // Reset throwForce to minimum throw force value
+        throwForce = minThrowForce;
+        charging = false;
+
+        // Clear the charging line renderer
+        ClearThrowLine();
     }
     
+    // If throwForce is >= maxThrowForce, throw the object (keeps players from charging button forever)
+    // Else, increase throwForce by chargeRate
+    private void ChargeThrow()
+    {
+        DrawThrowLine();
+        if (throwForce >= maxThrowForce)
+        {
+            Throw();
+        }
+        else
+        {
+            throwForce += chargeRate;
+        }
+    }
+
+    // Draw line that indicates how much force a throw will have
+    private void DrawThrowLine()
+    {
+        // Check if there is already a LineRenderer component in the holdPosition object
+        // If not, create a new one and set its attributes.
+        LineRenderer line = holdPosition.gameObject.GetComponent<LineRenderer>();
+        if (!line)
+        { 
+            line = holdPosition.gameObject.AddComponent<LineRenderer>(); // Add LineRenderer component to holdPosition
+            line.positionCount = 2; // How many points in the line
+            line.useWorldSpace = true; // If the line's positions are treated as world coordinates or relative to the game object its a part of
+            line.startWidth = 0.1f; // Width of start of the line
+            line.endWidth = 0.15f; // Width of end of the line
+            line.numCapVertices = 10; // Number of vertices at the ends (affects how round the ends are)
+            line.SetPosition(0, holdPosition.transform.position); // Position of first point in line
+            line.material = new Material(Shader.Find("Sprites/Default")); // Sets line's material to default material (so we can add colors to it, otherwise it defaults to gross pink)
+            line.startColor = new Color(255, 212, 0); // Color for the start of the line (not working)
+            line.endColor = new Color(255, 0, 0); // Color for the end of the line (not working)
+        }
+        else
+        {
+            line.SetPosition(0, holdPosition.transform.position); // Set position of the start of the line every frame so it moves with the player
+            line.SetPosition(1, line.GetPosition(0) + transform.parent.forward * (throwForce / 15)); // Set position of end of the line (simply add a vector relative to the throwForce so it grows every frame)
+            line.endWidth += 0.0025f; // Increase the width of the end to make it thicker the longer it charges.
+        }
+    }
+
+    // Destroy LineRenderer component that shows the charge line
+    private void ClearThrowLine()
+    {
+        LineRenderer line = holdPosition.gameObject.GetComponent<LineRenderer>();
+        if (line != null)
+        {
+            Destroy(line);
+        }
+    }
 }
