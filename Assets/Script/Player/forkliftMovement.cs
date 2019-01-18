8f69319b55e14dc0e0b73fe4f905b68fb49b3b33
Talon Baker <taalonn@gmail.com>
2019-01-18 14:23:31 -0800

    Forklift is functional. Rear wheel still needs some work. I know the problem. I just have to figure out a soulution. Forklift forks currently do not interact with boxes / pallets. yet.

--- Assets/Script/Player/forkliftMovement.cs
+++ Assets/Script/Player/forkliftMovement.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* Forklift movement
+    Movement is modeled after a single, rear-wheel drive forklift.
+    The pivot point is in the center of the forklift body.
+*/
 public class forkliftMovement : MonoBehaviour {
 
-    private float rearWheelRotation = 0.0f; // Wheel rotation between [85 deg, -85 deg]
+    public float rearWheelRotation = 0.0f; // Wheel rotation between [85 deg, -85 deg]
     private float maxWheelRotation = 85.0f; // +-85 degrees from 0 is max rear wheel rotation
     public float wheelRotationAmt = 0.5f;
 
-    private float currentSpeed = 0.0f; // How fast the forklift is currently moving in either direction
-    public float IncSpeedForwardAmt = 0.1f; // How fast the forklift can speed up going forward
-    public float IncSpeedBackwardsAmt = 0.05f; // How fast the forklift can speed up going backwards
+    public float currentSpeed = 0.0f; // How fast the forklift is currently moving in either direction
+    public float incrementSpeedAmt = 0.05f; // How fast the forklift can speed up going forward
 
-    public float maxForwardSpeed = 15.0f;
-    public float maxBackwardSpeed = -5.0f;
-    public float slowDownAmt = 0.01f; // How fast the forklift will return to a resting states
+    public float maxForwardSpeed = 1.0f;
+    public float maxBackwardSpeed = -1.0f;
+    public float slowDownAmt = 0.005f; // How fast the forklift will return to a resting states
+    public float turningDeadZone = 35.0f; // Maximum speed output between +-turning dead zone. Meaning speed will remain at full output while the wheel is between +-35 deg.
 
     private Transform rearWheel;
     private Rigidbody rb;
@@ -28,18 +32,16 @@ public class forkliftMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         // Moving forklift forward
-        if (Input.GetKey("w")){
+        if (Input.GetKey("w")) {
             if (currentSpeed >= maxForwardSpeed) {
                 currentSpeed = maxForwardSpeed;
             }
             else {
-                currentSpeed += IncSpeedForwardAmt;
-
-                // Moving forklift forward
+                currentSpeed += incrementSpeedAmt;
             }
         }
         // Turning rear wheel counter clockwise (left, towards +90deg) by wheel rotation amount 
-        if (Input.GetKey("a")) {
+        if (Input.GetKey("d")) {
             if(rearWheelRotation >= maxWheelRotation) {
                 rearWheelRotation = maxWheelRotation;
             }
@@ -58,13 +60,11 @@ public class forkliftMovement : MonoBehaviour {
                 currentSpeed = maxBackwardSpeed;
             }
             else {
-                currentSpeed -= IncSpeedBackwardsAmt;
-
-                
+                currentSpeed -= incrementSpeedAmt;
             }
         }
         // Turning rear wheel clockwise (right, towards -90deg) by wheel rotation amount 
-        if (Input.GetKey("d")) {
+        if (Input.GetKey("a")) {
             if (rearWheelRotation <= -maxWheelRotation) {
                 rearWheelRotation = -maxWheelRotation;
             }
@@ -78,10 +78,10 @@ public class forkliftMovement : MonoBehaviour {
             // Debug.Log("rearWheelRotation: " + rearWheelRotation);
         }
 
-        if (currentSpeed > 0.0f) {
+        if (currentSpeed >= 0.01f) {
             currentSpeed -= slowDownAmt;
         }
-        else if(currentSpeed < 0.0f) {
+        else if(currentSpeed <= -0.01f) {
             currentSpeed += slowDownAmt;
         }
         else {
@@ -95,11 +95,22 @@ public class forkliftMovement : MonoBehaviour {
 
 	} // End - update
 
-    // Forward movement is calculated through a combination of current wheel angle and movement speed
-    // If wheel angle is 0 (completely forward), movement speed is 100% forward
-    // If wheel angle is an angle other than 0, movement speed will be reduced by
+    /* Forward movement is calculated through a combination of current wheel angle and movement speed.
+        If wheel angle is 0 degrees (completely forward), movement speed is 100% forward.
+        However, if wheel angle is an angle other than 0 degrees, movement speed will be reduced by
+        that ratio and translated into rotation, instead.
+    */
     void moveForklift() {
-        transform.position += transform.forward * Time.deltaTime * currentSpeed;
+        // Applying the forklift's calculated forward movement
+        if(rearWheelRotation <= turningDeadZone && rearWheelRotation >= -turningDeadZone) {
+            this.transform.position += transform.forward * Time.deltaTime * currentSpeed * 2.5f;
+        }
+        else {
+            this.transform.position += transform.forward * Time.deltaTime * currentSpeed * (1 / (Mathf.Abs(rearWheelRotation / maxWheelRotation) / 1));
+        }
+
+        // Applying the forklift's calculated rotations
+        this.transform.Rotate(0.0f, (-rearWheelRotation/maxWheelRotation), 0.0f);
     }
 
 } // End - forkLiftMovement
