f4f6979508b985b6c5a64633444fc51dd2739186
DHalsey <dhalsey@ucsc.edu>
2019-01-18 16:16:14 -0800

    boxer prototype

---
+++ Assets/Script/Machinery/boxerSuction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxerSuction : MonoBehaviour {
+    public Vector3 forceDirection;
+    public float forcePower;
+	// Use this for initialization
+	void Start () {
+        forceDirection = forceDirection.normalized;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other) {
+        if(other.gameObject.tag == "grabbable") {
+            if (other.gameObject.GetComponent<Rigidbody>()) {
+                other.gameObject.GetComponent<Rigidbody>().AddForce(
+                    forceDirection * Time.deltaTime * forcePower,
+                    ForceMode.Force);
+            }
+        }
+    }
+}
